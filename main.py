ee6b5cadb7aaca9a0e4d69f83af31e8ff03499d0
fallingdeeper <arturstepaniuk@Arturs-MacBook-Air.local>
2021-06-01 01:21:22 +0300

    upd app

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import os
 
 from flask import Flask
 
-
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
     app = Flask(__name__)
 
     # a simple page that says hello
-    @app.route('/hello')
+    @app.route('/')
     def hello():
         return 'Olha Petryk\'s project.\nAt Jagiellonian University.'
 
     return app
+
+
+if __name__ == '__main__':
+    create_app().run(host='0.0.0.0', port=80)
